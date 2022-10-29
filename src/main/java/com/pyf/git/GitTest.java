c7ef3665cc974791ec695ae73a6314ac2d95ff8e
1012526675@qq.com <1012526675@qq.com>
2022-10-29 17:22:07 +0800

    hit-fix commit2

--- src/main/java/com/pyf/git/GitTest.java
+++ src/main/java/com/pyf/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello gi2!");
         System.out.println("hello gi3!");
         System.out.println("hello gi4!");
+        System.out.println("hot-fix test");
     }
 }
